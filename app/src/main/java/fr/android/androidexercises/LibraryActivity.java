eac997a2d34bd666aa0faf5bb6c831e9504c1f7f
Prygan <dimitrisaingre@orange.fr>
2017-11-09 15:22:23 +0100

    add date picker

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,5 +1,6 @@
 package fr.android.androidexercises;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -19,6 +22,7 @@ public class LibraryActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Button openButton = (Button) findViewById(R.id.openButton);
+        Button dateButton = (Button) findViewById(R.id.datepicker);
 
         final Book book = new Book("Garry Whopper", "CK Rowling");
 
@@ -29,6 +33,18 @@ public class LibraryActivity extends AppCompatActivity {
                 // TODO Add book to intent
                 intent.putExtra(BookActivity.BOOK, book);
                 startActivity(intent);
+            }
+        });
+
+        dateButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                new DatePickerDialog(LibraryActivity.this, new DatePickerDialog.OnDateSetListener(){
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                        Toast.makeText(LibraryActivity.this, dayOfMonth + "/" + monthOfYear + "/" + year,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }, 2015, 10, 07).show();
             }
         });
     }
