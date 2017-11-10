743a0166d175b4c3dad8e7d1649160c3a1bca98b
Prygan <dimitrisaingre@orange.fr>
2017-11-10 14:24:47 +0100

    add tests

--- app/src/test/java/fr/android/androidexercises/JUnitLoginActivityTest.java
+++ app/src/test/java/fr/android/androidexercises/JUnitLoginActivityTest.java
@@ -1,5 +1,35 @@
 package fr.android.androidexercises;
 
-public class JUnitLoginActivityTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
+public class JUnitLoginActivityTest {
+    @Mock LoginActivity activity;
+
+    @InjectMocks LoginPresenter presenter;
+
+    @Test
+    public void password_should_be_valid() throws Exception {
+        //Given
+        //When
+        presenter.checkCredentials("password");
+        //Then
+        Mockito.verify(activity).logged();
+        Mockito.verify(activity).message(R.string.text_logged);
+    }
+
+    @Test
+    public void password_should_be_not_valid() throws Exception {
+        //Given
+        //When
+        presenter.checkCredentials("p");
+        //Then
+        Mockito.verify(activity).notLogged();
+        Mockito.verify(activity).message(R.string.notLogged);
+    }
 }
